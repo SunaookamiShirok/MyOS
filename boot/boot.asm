ef0f0b319f58386dfd89004cf6c8c6bb9629cd65
SunaookamiShirok <WSY070502@outlook.com>
2026-07-01 23:06:55 +0800

    feat(vga): implement Lesson 06 v3 reusable print routine

--- boot/boot.asm
+++ boot/boot.asm
@@ -1,8 +1,8 @@
-;=========================================================
+;========================================
 ; MyOS Bootloader
-; Lesson 06 (Version 2)
-; Print String Using Loop
-;=========================================================
+; Lesson 06 (Version 3)
+; Reusable Print Routine
+;========================================
 
 org 0x7C00
 bits 16
@@ -11,56 +11,60 @@ start:
 
     cli
 
-    xor ax, ax
-    mov ds, ax
-    mov ss, ax
-    mov sp, 0x7C00
+    mov ax,0xB800
+    mov es,ax
 
-    ; Set text mode
-    mov ax, 0x0003
-    int 0x10
+    xor di,di
 
-    ; VGA memory
-    mov ax, 0xB800
-    mov es, ax
+    ; Print first string
+    mov si,message1
+    call print_string
 
-    xor di, di
-
-    ; SI -> message
-    mov si, message
-
-print_loop:
-
-    ; Read current character
-    mov al, [si]
-
-    ; End of string?
-    cmp al, 0
-    je hang
-
-    ; Write character
-    mov [es:di], al
-
-    ; White on black
-    mov byte [es:di+1], 0x0F
-
-    ; Next character
-    inc si
-
-    ; Next screen cell
-    add di, 2
-
-    jmp print_loop
+    ; Print second string
+    mov si,message2
+    call print_string
 
 hang:
+
     hlt
     jmp hang
 
-;---------------------------------------------------------
+;----------------------------------------
+; Print String Routine
+; ES -> VGA Memory
+; SI -> String
+; DI -> Screen Position
+;----------------------------------------
 
-message db "Hello MyOS",0
+print_string:
 
-;---------------------------------------------------------
+.next:
+
+    lodsb               ; AL = [SI]
+
+    cmp al,0
+    je .done
+
+    mov [es:di],al
+    inc di
+
+    mov byte [es:di],0x0F
+    inc di
+
+    jmp .next
+
+.done:
+
+    ret
+
+;----------------------------------------
+; Data
+;----------------------------------------
+
+message1 db "Hello, ",0
+message2 db "MyOS!",0
+
+;----------------------------------------
 
 times 510-($-$$) db 0
 dw 0xAA55
